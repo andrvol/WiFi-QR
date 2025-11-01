77c0095571d37c3cf8481fc4b9f0b85bef882eab
andreyvolobuiev <Andvo1007@gmail.com>
2025-11-01 12:21:07 +0200

    -

---
+++ WiFi_QRCode_App/WiFi_QRCode_App/Program.cs
@@ -0,0 +1,19 @@
+﻿using QRCoder;
+
+var wifiPayload = new PayloadGenerator.WiFi
+(
+    ssid: "MyWiFi",
+    password: "123456",
+    authenticationMode: PayloadGenerator.WiFi.Authentication.WPA
+);
+string qrText = wifiPayload.ToString();
+
+using var generator = new QRCodeGenerator();
+
+QRCodeData data = generator. CreateQrCode (
+    qrText, QRCodeGenerator. ECCLevel. Q);
+
+using var qrCode = new PngByteQRCode (data) ;
+byte[] pngBytes = qrCode. GetGraphic (20) ;
+
+File.WriteAllBytes("wifi.png", pngBytes);
